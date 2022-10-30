3c978d5a46e89719803f2945530927cc7dfed52e
ItsCoolGAMER959 <53452072+ItsCoolGAMER959@users.noreply.github.com>
2022-10-30 15:07:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+window.addEventListener("load", function () {
+  const form = document.getElementById("my-form");
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+    const data = new FormData(form);
+    const action = e.target.action;
+    fetch(action, {
+      method: "POST",
+      body: data
+    });
+  });
+});
+$("form").on("submit", function (e) {
+  e.preventDefault();
+  $(".child").remove();
+  $(".child2").show();
+});
